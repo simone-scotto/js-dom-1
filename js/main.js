684c19ac495490b80987658212e126ffd721eeeb
simone-scotto <simone.scottotkd@gmail.com>
2025-06-05 17:27:29 +0200

    button color, cantered

--- js/main.js
+++ js/main.js
@@ -7,9 +7,13 @@ buttonEl.addEventListener("click", function () {
     imgEl.src = ".//img/yellow_lamp.png";
     imgEl.style.transform = "scaleX(-1)";
     buttonEl.textContent = "spegnimi";
+    imgEl.style.marginLeft = "3%";
+    buttonEl.style.backgroundColor = "red";
   } else {
     imgEl.src = ".//img/white_lamp.png";
     imgEl.style.transform = "scaleX(1)";
     buttonEl.textContent = "accendimi";
+    imgEl.style.marginLeft = "2%";
+    buttonEl.style.backgroundColor = "green";
   }
 });
