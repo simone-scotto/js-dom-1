2282590726c680e98a6a5df391e442b1f1330d91
simone-scotto <simone.scottotkd@gmail.com>
2025-06-05 16:33:42 +0200

    if else method

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ const buttonEl = document.getElementById("bottone");
 const imgEl = document.querySelector("img");
 
 buttonEl.addEventListener("click", function () {
-  imgEl.src = ".//img/yellow_lamp.png";
-  imgEl.style.transform = "scaleX(-1)";
+  if (buttonEl.textContent === "accendimi") {
+    imgEl.src = ".//img/yellow_lamp.png";
+    imgEl.style.transform = "scaleX(-1)";
+    buttonEl.textContent = "spegnimi";
+  } else {
+    imgEl.src = ".//img/white_lamp.png";
+    imgEl.style.transform = "scaleX(1)";
+    buttonEl.textContent = "accendimi";
+  }
 });
